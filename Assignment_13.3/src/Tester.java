ab79f5ed72f330d85b4e23736bbcafe889d1dcac
Ashish221b <ashishgupta199736.ag@gmail.com>
2023-10-21 16:41:04 +0530

    Assignment 13.3 uploaded

---
+++ Assignment_13.3/src/Tester.java
@@ -0,0 +1,28 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		System.out.println("Program Started.");
+		Scanner sc = new Scanner(System.in);
+		
+		try(FileWriter fwr = new FileWriter("file.txt")) {
+			try(BufferedWriter bwr = new BufferedWriter(fwr)) {
+				for(int i=1; i<=4; i++) {
+					System.out.print("Enter the "+i+"th line :");
+					String line = sc.nextLine();
+					bwr.write(line);
+				}
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("File Saved.");
+	}
+}
